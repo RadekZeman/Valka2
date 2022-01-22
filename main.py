9984fb270597eb2534698f0fcf10c4ebda38de14
ravycz <radek2005@centrum.cz>
2022-01-22 17:34:44 +0100

    pro 2.0

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
  print(f"hrac2={hrac2}")
  a = 0
  b = 0
- z = 0
  while not(len(hrac1) == 0 or len(hrac2) == 0):
     if len(hrac1) == 0:
         print(f"hrac2 vyhral")
@@ -40,36 +39,40 @@ if __name__ == '__main__':
         c = 0
         d = 1
         while hrac1[a] == hrac2[b]:
-            if len(hrac1) == 3 + c:
-                c = c-1
-            if len(hrac1) == 2 + c:
-                c = c-2
-            if len(hrac1) == 1 + c:
-                print(f"hrac1 malo karet, hrac2 vyhral")
-                z = 1
-            if len(hrac2) == 3 + c:
-                c = c - 1
-            if len(hrac2) == 2 + c:
-                c = c - 2
-            if len(hrac2) == 1 + c:
-                print(f"hrac2 malo karet, hrac1 vyhral")
-                z = 1
-            c += 3
-            d += 3
-            if hrac1[c] > hrac2[c]:
-                hrac1 = np.append(hrac1, hrac1[: d])
-                hrac1 = np.append(hrac1, hrac2[: d])
-                hrac1 = hrac1[d:]
-                hrac2 = hrac2[d:]
-                print(f"hrac1={hrac1}")
-                print(f"hrac2={hrac2}")
-            elif hrac2[c] > hrac1[c]:
-                hrac2 = np.append(hrac2, hrac1[: d])
-                hrac2 = np.append(hrac2, hrac2[: d])
-                hrac1 = hrac1[d:]
-                hrac2 = hrac2[d:]
-                print(f"hrac1={hrac1}")
-                print(f"hrac2={hrac2}")
+                if len(hrac1) == 3 + c:
+                    c = c-1
+                if len(hrac1) == 2 + c:
+                    c = c-2
+                if len(hrac1) == 1 + c:
+                    print(f"hrac1 malo karet, hrac2 vyhral")
+                    hrac1 = []
+                    break
+                if len(hrac2) == 3 + c:
+                    c = c - 1
+                if len(hrac2) == 2 + c:
+                    c = c - 2
+                if len(hrac2) == 1 + c:
+                    print(f"hrac2 malo karet, hrac1 vyhral")
+                    hrac2 = []
+                    break
+                c += 3
+                d = c + 1
+                if hrac1[c] > hrac2[c]:
+                    hrac1 = np.append(hrac1, hrac1[: d])
+                    hrac1 = np.append(hrac1, hrac2[: d])
+                    hrac1 = hrac1[d:]
+                    hrac2 = hrac2[d:]
+                    print(f"hrac1={hrac1}")
+                    print(f"hrac2={hrac2}")
+                    break
+                elif hrac2[c] > hrac1[c]:
+                    hrac2 = np.append(hrac2, hrac1[: d])
+                    hrac2 = np.append(hrac2, hrac2[: d])
+                    hrac1 = hrac1[d:]
+                    hrac2 = hrac2[d:]
+                    print(f"hrac1={hrac1}")
+                    print(f"hrac2={hrac2}")
+                    break
  if len(hrac1) == 0:
     print(f"hrac2 vyhral")
  if len(hrac2) == 0:
